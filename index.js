f0d77950a130b84132cc3995ac9341a9140b3b7e
seongeon <dbtjsrjs8282@naver.com>
2023-02-02 01:55:05 +0900

    vite setting

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log("listening on 8080");
 });
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+// app.get('*', function(request, response){
+//   response.sendFile( path.join(__dirname, 'react-project/build/index.html') )
+// });
+
+app.get("/", function (request, response) {
+  response.sendFile(path.join(__dirname, "public"));
 });
 
 app.get("/get", function (req, res) {
-  res.send("get 페이지 입니다.");
+  res.send("get 페이지 입니다dd.");
 });
 
 module.exports = app;
